faabb586df02382898a8a8a4679abc51d33079e8
lazar3s <peryou69@gmail.com>
2023-05-10 13:31:34 +0400

    playerr movement final

--- mejadalada new/Assets/Scenes/Scripts/move.cs
+++ mejadalada new/Assets/Scenes/Scripts/move.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    private BoxCollider2D boxcollider;
+    private BoxCollider2D boxcolider;
     private Vector3 movedelta;
     private RaycastHit2D rayhit;
     public float speed = 1.0f;
+    
     private void Start()
     {
-        boxcollider = GetComponent<BoxCollider2D>();
+        boxcolider = GetComponent<BoxCollider2D>();
     }
 
     
-    private void fixedupdate()
+        private void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         
 
         // to reset movement 
-        movedelta = new Vector3(x, y, 0);
+        movedelta = new Vector3(x,y,0);
 
         // to swap sprite direction from left or right
 
@@ -30,13 +31,13 @@ public class move : MonoBehaviour
         else if (movedelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        rayhit = Physics2D.BoxCast(transform.position, boxcollider.size, 0, new Vector2(0, movedelta.y), Mathf.Abs(movedelta.y * Time.deltaTime), LayerMask.GetMask("player", "blocking"));
+        rayhit = Physics2D.BoxCast(transform.position, boxcolider.size, 0, new Vector2(0, movedelta.y), Mathf.Abs(movedelta.y * Time.deltaTime), LayerMask.GetMask("player", "blocking"));
         if (rayhit.collider == null)
         {
             // make player move
             transform.Translate(0, movedelta.y * Time.deltaTime * speed, 0);
         }
-        rayhit = Physics2D.BoxCast(transform.position, boxcollider.size, 0, new Vector2(movedelta.x, 0), Mathf.Abs(movedelta.x * Time.deltaTime), LayerMask.GetMask("player", "blocking"));
+        rayhit = Physics2D.BoxCast(transform.position, boxcolider.size, 0, new Vector2(movedelta.x, 0), Mathf.Abs(movedelta.x * Time.deltaTime), LayerMask.GetMask("player", "blocking"));
         if (rayhit.collider == null)
         {
             transform.Translate(movedelta.x * Time.deltaTime * speed, 0, 0);
