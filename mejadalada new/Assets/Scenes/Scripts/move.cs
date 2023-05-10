d23c984de1e61872cbcf881bcf1fd9b2ed77bd9d
lazar3s <peryou69@gmail.com>
2023-05-10 13:20:53 +0400

    my movement

--- mejadalada new/Assets/Scenes/Scripts/move.cs
+++ mejadalada new/Assets/Scenes/Scripts/move.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    private CircleCollider2D circleCollider;
+    private BoxCollider2D boxcollider;
     private Vector3 movedelta;
-    private RaycastHit2D raycastHit;
+    private RaycastHit2D rayhit;
     public float speed = 1.0f;
     private void Start()
     {
-        circleCollider = GetComponent<CircleCollider2D>();
+        boxcollider = GetComponent<BoxCollider2D>();
     }
 
     
@@ -18,8 +18,29 @@ public class move : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+        
 
+        // to reset movement 
+        movedelta = new Vector3(x, y, 0);
 
+        // to swap sprite direction from left or right
+
+        if (movedelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (movedelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        rayhit = Physics2D.BoxCast(transform.position, boxcollider.size, 0, new Vector2(0, movedelta.y), Mathf.Abs(movedelta.y * Time.deltaTime), LayerMask.GetMask("player", "blocking"));
+        if (rayhit.collider == null)
+        {
+            // make player move
+            transform.Translate(0, movedelta.y * Time.deltaTime * speed, 0);
+        }
+        rayhit = Physics2D.BoxCast(transform.position, boxcollider.size, 0, new Vector2(movedelta.x, 0), Mathf.Abs(movedelta.x * Time.deltaTime), LayerMask.GetMask("player", "blocking"));
+        if (rayhit.collider == null)
+        {
+            transform.Translate(movedelta.x * Time.deltaTime * speed, 0, 0);
+        }
 
     }
 }
